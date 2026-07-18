a7d0a4cb9e18d06bd20f83b2d4fffcc2d977125a
Eric-Guo <eric.guocz@gmail.com>
2026-07-18 20:25:40 +0800

    Add configurable document title updates

--- src/entry.tsx
+++ src/entry.tsx
@@ -1,10 +1,12 @@
 import { createEffect } from "solid-js"
 import { render } from "solid-js/web"
 import { AppBaseProviders, AppInterface } from "@/app"
+import { SET_DOCUMENT_TITLE } from "@/constants/session"
 import { currentLocalAgent } from "@/context/server-session-store"
 
 function Root() {
   createEffect(() => {
+    if (!SET_DOCUMENT_TITLE) return
     document.title = currentLocalAgent()
   })
   return (
